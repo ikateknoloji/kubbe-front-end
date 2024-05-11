aa55583282c2d0a4233355f930b864b7c2d038ba
Kaan Kara <ikateknoloji@gmail.com>
2024-05-11 16:03:52 +0300

    Global bileşenler oluşturuldu

---
+++ src/components/CoverContent.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="flex flex-col p-2 md:p-5 box box--stacked mb-10 relative">
+    <div class="pb-5 mb-6 font-medium border-b border-dashed border-slate-300/70 text-[0.94rem]">{{ title }}</div>
+    <div>
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: ''
+  }
+});
+
+</script>
+
+<style scoped>
+.box {
+  box-shadow: 0 3px 5px #0000000b;
+  background-color: #fff;
+  border: 1px solid #e2e8f0;
+  border-radius: .6rem;
+  position: relative;
+}
+
+.box--stacked:before {
+  content: "";
+  z-index: -1;
+  box-shadow: 0 1px 2px #0000000d;
+  height: 100%;
+  background-color: #f8fafc;
+  border: 1px solid #e2e8f0;
+  position: absolute;
+  border-radius: .5rem;
+  left: .875rem;
+  right: .875rem;
+  top: 0;
+  margin-top: .75rem;
+}
+</style>
---
+++ src/components/PageTitle.vue
@@ -0,0 +1,13 @@
+<template>
+  <h3 class="text-black-800 text-2xl lg:text-4xl tracking-[-1.5px] font-poppins font-medium py-10 px-4">
+    {{ text }}
+  </h3>
+</template>
+
+<script setup>
+const props = defineProps({
+  text: String
+})
+</script>
+
+<style scoped></style>
