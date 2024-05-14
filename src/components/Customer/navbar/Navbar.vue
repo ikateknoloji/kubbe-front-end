3411d65e4d28e43a46bf0b274edc9a67f5bb0da5
Kaan Kara <ikateknoloji@gmail.com>
2024-05-14 14:02:02 +0300

    Navbar bileşeni oluşturuldu.

---
+++ src/components/Customer/navbar/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav class="hs-accordion-group p-6 w-full flex flex-col  flex-wrap font-poppins font-medium"
+    data-hs-accordion-always-open>
+    <ul>
+      <li class="mb-10">
+        <InformationLink />
+      </li>
+      <li class="mb-10">
+        <router-link to="/dashboard/musteri/order-create"
+          class="inline-block w-[200px] mx-4 select-none rounded-lg bg-blue-500 py-4 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+          Sipariş Oluştur
+        </router-link>
+      </li>
+      <li class="mb-10">
+        <OrderLink />
+        <PaymentLink />
+        <DeliveryLink />
+        <RejectedOrders />
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script setup>
+import InformationLink from './links/InformationLink.vue';
+import OrderLink from './links/OrderLink.vue';
+import PaymentLink from './links/PaymentLink.vue';
+import DeliveryLink from './links/DeliveryLink.vue';
+import RejectedOrders from './links/RejectedOrders.vue';
+
+const props = defineProps({
+  status: Object
+});
+
+
+
+</script>
+
+<style scoped></style>
---
+++ src/components/Customer/navbar/links/InformationLink.vue
@@ -0,0 +1,66 @@
+<template>
+  <li>
+    <ul class="space-y-2">
+      <li>
+        <router-link to="/dashboard"
+          class="flex items-center  py-2 px-2.5 bg-gray-100 text-sm text-slate-700 rounded-lg hover:bg-gray-300 ">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+            stroke="currentColor" class="w-6 h-6 mr-3">
+            <path stroke-linecap="round" stroke-linejoin="round"
+              d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75" />
+          </svg>
+          Yönetim Paneli
+        </router-link>
+      </li>
+
+      <li>
+        <router-link to="/"
+          class="flex items-center  py-2 px-2.5 bg-gray-100 text-sm text-slate-700 rounded-lg hover:bg-gray-300 ">
+          <div class="w-full flex items-center justify-between">
+            <div class="flex items-center w-full">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="w-6 h-6 mr-3">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                  d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0M3.124 7.5A8.969 8.969 0 015.292 3m13.416 0a8.969 8.969 0 012.168 4.5" />
+              </svg>
+              Bildirimler
+            </div>
+            <div class="relative flex items-center justify-center mr-2">
+              <span class="absolute w-8 h-8 p-2 text-xs rounded bg-red-500 text-white "></span>
+              <span class="z-10 text-white">9</span>
+            </div>
+          </div>
+        </router-link>
+      </li>
+
+      <li>
+        <router-link to="/dashboard/musteri/user"
+          class="flex items-center  py-2 px-2.5 bg-gray-100 text-sm text-slate-700 rounded-lg hover:bg-gray-300 ">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+            stroke="currentColor" class="w-6 h-5 mr-3">
+            <path stroke-linecap="round" stroke-linejoin="round"
+              d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+          </svg>
+          Kullanıcı İşlemleri
+        </router-link>
+      </li>
+
+      <li>
+        <router-link to="/dashboard/musteri/orders-delayed"
+          class="flex items-center  py-2 px-2.5 bg-gray-100 text-sm text-slate-700 rounded-lg hover:bg-gray-300 ">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+            stroke="currentColor" class="w-6 h-5 mr-3">
+            <path stroke-linecap="round" stroke-linejoin="round"
+              d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
+          </svg>
+          Beklemedeki Siparişler
+        </router-link>
+      </li>
+
+    </ul>
+  </li>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
