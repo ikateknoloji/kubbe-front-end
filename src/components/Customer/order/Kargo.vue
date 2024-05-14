9fd14935fef9809d74274d7271fdfe30d19be216
Kaan Kara <ikateknoloji@gmail.com>
2024-05-14 14:02:56 +0300

    Sipariş listeleri oluşturuldu.

---
+++ src/components/Customer/order/Kargo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <PageTitle text="Teslim Edilen Siparişler" />
+    <Cards :order="order" />
+  </div>
+
+</template>
+
+<script setup>
+import PageTitle from '@/components/PageTitle.vue';
+import Cards from '@/components/Customer/Cards.vue';
+
+const props = defineProps({
+  order: Object
+});
+
+</script>
+
+<style scoped></style>
---
+++ src/components/Customer/order/UrunHazir.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <PageTitle text="Hazır Olan Siparişler" />
+    <Cards :order="order" />
+  </div>
+
+</template>
+
+<script setup>
+import PageTitle from '@/components/PageTitle.vue';
+import Cards from '@/components/Customer/Cards.vue';
+
+const props = defineProps({
+  order: Object
+});
+
+</script>
+
+<style scoped></style>
